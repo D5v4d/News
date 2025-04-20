857f51799b245abbc5def86dfd70266ceb43a5a1
D5v4d <166731621+D5v4d@users.noreply.github.com>
2025-04-20 21:35:44 +0300

    коментарий

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/News/',
   build: {
     outDir: 'docs', // Изменили на docs для GitHub Pages
   },
